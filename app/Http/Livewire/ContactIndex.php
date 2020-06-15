6f63b94614323d5e5b13a12b876b6171f0ee4179
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2020-06-15 08:12:03 +0800

    make dynamic paginate

--- app/Http/Livewire/ContactIndex.php
+++ app/Http/Livewire/ContactIndex.php
@@ -11,6 +11,7 @@ class ContactIndex extends Component
     use WithPagination;
 
     public $statusUpdate = false;
+    public $paginate = 5;
     
     protected $listeners = [
         'contactStored' => 'handleStore',
@@ -20,7 +21,7 @@ class ContactIndex extends Component
     public function render()
     {
         return view('livewire.contact-index', [
-             'contacts' => Contact::latest()->paginate(5)     
+             'contacts' => Contact::latest()->paginate($this->paginate)
         ]);
     }
 
